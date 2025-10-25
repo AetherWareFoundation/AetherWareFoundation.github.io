3306cee57cdf0c57504f5bf36acf7caf25556204
Philipp Molitor <philipp@molitor-consulting.com>
2025-10-25 12:13:45 +0200

    more improvements

--- src/components/mdx/Showcase.tsx
+++ src/components/mdx/Showcase.tsx
@@ -18,12 +18,14 @@ export type ShowcaseProps = {
 
 export const Showcase = ({ title, icon, children }: ShowcaseProps) => {
   return (
-    <div className="flex gap-6 items-start">
-      <div className="flex-shrink-0 size-6 lg:size-10 flex items-center justify-center [&>svg]:text-(--doc-color-lib)">
+    <div className="flex gap-6 lg:gap-4 items-start">
+      <div className="flex-shrink-0 size-8 flex items-center justify-center [&>svg]:size-full [&>svg]:text-(--doc-color-lib)">
         {icon}
       </div>
       <div className="flex flex-col gap-2 flex-1">
-        <span className="text-lg font-semibold text-white">{title}</span>
+        <span className="text-lg font-semibold text-fd-foreground">
+          {title}
+        </span>
         <div className="text-md text-fd-muted-foreground leading-relaxed [&>*:first-child]:mt-0 [&>*:last-child]:mb-0">
           {children}
         </div>
