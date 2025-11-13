187d3a52fe724d3fc31754fbd5e6a5e0ed08f55a
Philipp Molitor <philipp@molitor-consulting.com>
2025-11-13 22:31:50 +0100

    add c15t

--- src/components/root/Providers.tsx
+++ src/components/root/Providers.tsx
@@ -2,12 +2,15 @@ import type { ReactNode } from "react";
 
 import { RootProvider } from "fumadocs-ui/provider";
 
+import { ConsentProvider } from "./ConsentProvider";
 import { StaticSearchDialog } from "@/components/root/StaticSearchDialog";
 
 export function Providers({ children }: { children: ReactNode }) {
   return (
-    <RootProvider search={{ SearchDialog: StaticSearchDialog }}>
-      {children}
-    </RootProvider>
+    <ConsentProvider>
+      <RootProvider search={{ SearchDialog: StaticSearchDialog }}>
+        {children}
+      </RootProvider>
+    </ConsentProvider>
   );
 }
