36a2505a2aa47e5c285e17e77a1d810b0b7f6723
Philipp Molitor <philipp@molitor-consulting.com>
2025-11-14 11:09:27 +0100

    fix

--- src/instrumentation-client.ts
+++ src/instrumentation-client.ts
@@ -2,10 +2,13 @@ import { init } from "@plausible-analytics/tracker";
 
 import { IS_LIVE, PLAUSIBLE_ENDPOINT, SITE_HOST } from "@/config";
 
-// set up plausible analytics
 if (PLAUSIBLE_ENDPOINT && IS_LIVE) {
+  console.log(
+    "initializing plausible analytics - don't worry, we collect absolutely no personal data!",
+  );
   init({
-    domain: SITE_HOST.replaceAll(/:\d+/, ""),
+    domain: SITE_HOST.replaceAll(/:\d+/g, ""),
+    endpoint: PLAUSIBLE_ENDPOINT,
     captureOnLocalhost: false,
     outboundLinks: true,
     fileDownloads: true,
